dedf4fccd8afae5dddf5538c04e428c018c248bc
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-08-07 11:54:44 +0400

    refactor: :recycle: update pattern matching logic and improve async handling

--- src/Program.cs
+++ src/Program.cs
@@ -1,37 +1,31 @@
-using System;
-using System.IO;
-
-static bool MatchPattern(string inputLine, string pattern)
-{
-    if (pattern.Length == 1)
-    {
-        return inputLine.Contains(pattern);
-    }
-    else
-    {
-        throw new ArgumentException($"Unhandled pattern: {pattern}");
-    }
-}
-
 if (args[0] != "-E")
 {
     Console.WriteLine("Expected first argument to be '-E'");
     Environment.Exit(2);
 }
 
-string pattern = args[1];
-string inputLine = Console.In.ReadToEnd();
+var pattern = args[1];
+var inputLine = await Console.In.ReadToEndAsync();
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.Error.WriteLine("Logs from your program will appear here!");
+await Console.Error.WriteLineAsync("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 
 if (MatchPattern(inputLine, pattern))
-{
     Environment.Exit(0);
-}
 else
-{
     Environment.Exit(1);
+
+return;
+
+static bool MatchPattern(string inputLine, string pattern)
+{
+    if (pattern.Length < 1) throw new ArgumentException($"Unhandled pattern: {pattern}");
+
+    return pattern switch
+    {
+        "\\d" => inputLine.Any(char.IsDigit),
+        _ => false
+    };
 }
